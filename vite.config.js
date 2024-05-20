338104e58e4edb4244bdca5e7cfb1ca1868f5c47
RazinTeqB <razin@teqbuddies.com>
2024-05-20 09:52:18 +0530

    vite config toggle clean prod build options with env varisble

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { transform } from "esbuild";
 
 function minifyEs() {
@@ -20,56 +20,63 @@ function minifyEs() {
     };
 }
 
-export default defineConfig({
-    esbuild: {
-        drop: ['console', 'debugger']
-    },
-    plugins: [minifyEs()], // TODO: Disabled for development purpose only.
-    build: {
-        // emptyOutDir: false,
-        outDir: "./dist",
-        assetsDir: "assets",
-        minify: "terser",
-        lib: {
-            entry: "main.js", // Main entry file
-            name: "mkd-plugin",
-            // formats: ["es", "umd"], // Output formats: ES module and UMD
-            formats: ["es"], // Output formats: ES module and UMD
-            fileName: (format) => `mkd-plugin.${format}.js`,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    const isPROD = env?.VITE_NODE_ENVIRONMENT === "production";
+
+    return {
+        esbuild: {
+            drop: isPROD ? ["console", "debugger"] : [],
         },
-        rollupOptions: {
-            // External dependencies to exclude from the bundle
-            external: [],
-            output: {
-                inlineDynamicImports: false, // should be false for code splitting
-                assetFileNames: (assetInfo) => {
-                    if (assetInfo.name.endsWith(".css")) {
-                        return "assets/css/[name][extname]";
-                    } else {
-                        return "[name][extname]";
-                    }
-                },
-                // Global variables to use in UMD format for external imports
-                globals: {},
-                manualChunks(id) {
-                    if (id.includes("node_modules")) {
-                        return "@vendor";
-                    } else if (id.includes("/src/templates/")) {
-                        // Check if file is inside the templates directory
-                        // Extract folder name and use it as chunk name
-                        const match = id.match(/src\/templates\/([^\/]+)\//);
-                        if (match) {
-                            return `${match[1]}`; // This groups all files in the same sub-directory into one chunk
+        plugins: isPROD ? [minifyEs()] : [], // TODO: Disabled for development purpose only.
+        build: {
+            emptyOutDir: isPROD,
+            outDir: "./dist",
+            assetsDir: "assets",
+            minify: "terser",
+            lib: {
+                entry: "main.js", // Main entry file
+                name: "mkd-plugin",
+                // formats: ["es", "umd"], // Output formats: ES module and UMD
+                formats: ["es"], // Output formats: ES module and UMD
+                fileName: (format) => `mkd-plugin.${format}.js`,
+            },
+            rollupOptions: {
+                // External dependencies to exclude from the bundle
+                external: [],
+                output: {
+                    inlineDynamicImports: false, // should be false for code splitting
+                    assetFileNames: (assetInfo) => {
+                        if (assetInfo.name.endsWith(".css")) {
+                            return "assets/css/[name][extname]";
+                        } else {
+                            return "[name][extname]";
                         }
-                    }
+                    },
+                    // Global variables to use in UMD format for external imports
+                    globals: {},
+                    manualChunks(id) {
+                        if (id.includes("node_modules")) {
+                            return "@vendor";
+                        } else if (id.includes("/src/templates/")) {
+                            // Check if file is inside the templates directory
+                            // Extract folder name and use it as chunk name
+                            const match = id.match(
+                                /src\/templates\/([^\/]+)\//
+                            );
+                            if (match) {
+                                return `${match[1]}`; // This groups all files in the same sub-directory into one chunk
+                            }
+                        }
+                    },
                 },
             },
+            cssCodeSplit: false,
         },
-        cssCodeSplit: false,
-    },
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src"),
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "src"),
+            },
         },
-    },
+    };
 });
